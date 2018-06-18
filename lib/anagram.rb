b2aac9a7c88a61a5c58f48eb7709aae8a7cdc8f1
Anusha Konatala <anu.konatala@gmail.com>
2018-06-18 20:47:35 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+attr_accessor :word
+
+def initialize(word)
+@word = word
+end
+
+def match(words)
+  match = ""
+  words.each do |word| 
+    if word.sort == @word.sort  
+      match =	word
+    end
+  end
+
+  match
+  
+end
+
+end
