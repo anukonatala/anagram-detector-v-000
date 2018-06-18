bc1e6763d4217b69dd285b3dd9e3316404b59944
Anusha Konatala <anu.konatala@gmail.com>
2018-06-18 20:55:17 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,8 @@ end
 def match(words)
   match = []
   words.each do |i| 
-   if i.chars.sort.join == word.chars.sort.join
+  # if i.chars.sort.join == word.chars.sort.join
+  if i.split("").sort == word.split("").sort
     match << i
    end
   end
